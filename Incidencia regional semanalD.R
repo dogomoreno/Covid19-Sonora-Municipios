773c33493b366eab1eb720672ef496fa92919255
dogomoreno <dogomoreno@users.noreply.github.com>
2021-01-14 23:34:08 -0700

    14/01/2020
    Actualización

--- Incidencia regional semanalD.R
+++ Incidencia regional semanalD.R
@@ -55,8 +55,8 @@ paragraf <- theme(plot.title = (element_text(family = "Lato Black", size = 32, c
                   legend.title = element_text(family = "Lato Black", size = 8, color = "black"),
                   plot.caption = element_text(family = "Lato Light", size =10, color = "gray50"),
                   axis.title = element_text(family = "Lato", size = 12))
-subtitulo <- "Incidencia semanal de casos de covid-19\nCorte al lunes 11/01/2021"
-Semanalab <- "Semana de martes a lunes"
+subtitulo <- "Incidencia semanal de casos de covid-19\nCorte al miércoles 13/01/2021"
+Semanalab <- "Semanas de jueves a miércoles"
 marcas <- c( "+162", "59-162", "30-59","15-30", "0-15")
 
 #Río Sonora
