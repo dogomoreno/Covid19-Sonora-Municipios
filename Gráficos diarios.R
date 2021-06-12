e05085663a4e43af468d41305ccc738d9f0ca19c
dogomoreno <67255812+dogomoreno@users.noreply.github.com>
2021-06-11 20:30:36 -0700

    11/06/2021
    Actualización

--- Gráficos diarios.R
+++ Gráficos diarios.R
@@ -24,9 +24,9 @@ library("Cairo")
 library(directlabels)
 library(ggtext)
 
-Fechahoy <- "Corte al 10 de junio de 2021"
+Fechahoy <- "Corte al 11 de junio de 2021"
 fuente <- "Elaboración Luis Armando Moreno (@dogomoreno) con información de la Secretaría de Salud del Estado de Sonora\nwww.luisarmandomoreno.com"
-subtitulo <- "Casos confirmados en los últimos 7 días por 100 mil habitantes\nCorte al 10/06/2021"
+subtitulo <- "Casos confirmados en los últimos 7 días por 100 mil habitantes\nCorte al 11/06/2021"
 
 POBMUN <- read_csv("Bases/POBMUN.csv", col_types = cols(CVEGEO = col_character()), 
                    locale = locale(encoding = "ISO-8859-1"))
