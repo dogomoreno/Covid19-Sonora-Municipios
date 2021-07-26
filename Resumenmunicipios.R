519495968cac4e55601799d492f97b17d2b1fa9c
dogomoreno <67255812+dogomoreno@users.noreply.github.com>
2021-07-26 07:41:04 -0700

    26/07/2021
    Actualización

--- Resumenmunicipios.R
+++ Resumenmunicipios.R
@@ -29,7 +29,7 @@ rm(list=ls())
                                        MUNICIPIO = col_character(), NUEVOS = col_integer(), X1 = col_skip()), 
                       locale = locale(encoding = "ISO-8859-1"))
   
-  Fechahoy <- "Al reporte del 25 de julio de 2021"
+  Fechahoy <- "Al reporte del 26 de julio de 2021"
   fuente <- "Elaboración Luis Armando Moreno (@dogomoreno) con información de la Secretaría de Salud del Estado de Sonora\nPor continuidad, la fecha de corte se asume como la del día anterior al reporte. | www.luisarmandomoreno.com"
   
   POBMUN <- read_csv("Bases/POBMUN.csv", col_types = cols(CVEGEO = col_character()), 
