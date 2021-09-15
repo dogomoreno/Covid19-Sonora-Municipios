0feb7ff43a573ec56981d9f469103d5820c9eed4
dogomoreno <67255812+dogomoreno@users.noreply.github.com>
2021-09-14 17:02:56 -0700

    Update Gráficos diarios.R

--- scripts/Gráficos diarios.R
+++ scripts/Gráficos diarios.R
@@ -526,7 +526,7 @@ Hospitalizados <- Sonora.DF %>% ggplot(aes(x= Fecha, y= Hospitalizados)) +
   geom_point( data = subset(Sonora.DF , Fecha == max(Fecha)), fill="white", size=1 , shape=21, color="#F79646", stroke=1) +
   geom_dl( data = subset(Sonora.DF , Fecha == max(Fecha)), aes(label = Hospitalizados), color="#F79646", method = list(dl.trans(x = x + 0.2), "last.bumpup", cex = 1.5, fontfamily= "Lato Black")) +
   scale_y_continuous(expand = c(0, 0), limits= c(0,600), breaks=seq(0,600,50)) +
-  scale_x_date(expand=c(0,0), limits = c(as.Date("2020-08-01"), as.Date("2021-10-10")), date_breaks = "1 month", date_labels = "%B") +
+  scale_x_date(expand=c(0,0), limits = c(as.Date("2020-08-01"), max(as.Date(Sonora.DF$Fecha))+50), date_breaks = "1 month", date_labels = "%B") +
   coord_cartesian(expand = FALSE, clip = 'off') +
   theme_bw() +
   temaejes +
@@ -548,7 +548,7 @@ Activos <- Sonora.DF %>% ggplot() +
   geom_dl( data = subset(Sonora.DF , Fecha == max(Fecha)), aes(x= Fecha, y= Ambulatorios.Activos, label = Ambulatorios.Activos), color="#58BCBC", method = list(dl.trans(x = x + 0.2), "last.bumpup", cex = 1.5, fontfamily= "Lato Black")) +
   scale_color_manual(values= c("Tendencia promedio móvil 7 días"= "#58BCBC"))+
   scale_y_continuous(expand = c(0, 0), limits= c(0,6000), breaks=seq(0,6000,1000)) +
-  scale_x_date(expand=c(0,0), limits = c(as.Date("2020-06-19"), as.Date("2021-10-20")), date_breaks = "1 month", date_labels = "%B") +
+  scale_x_date(expand=c(0,0), limits = c(as.Date("2020-06-19"), max(as.Date(Sonora.DF$Fecha))+50), date_breaks = "1 month", date_labels = "%B") +
   coord_cartesian(expand = FALSE, clip = 'off') +
   theme_bw() +
   temaejes + 
